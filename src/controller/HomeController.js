33b4cfe36f219fcd61e53b814da09afa6ddb864a
kienpo <kienhungdung100702@gmail.com>
2024-12-27 16:17:50 +0700

    second change

--- src/controller/HomeController.js
+++ src/controller/HomeController.js
@@ -1,3 +1,6 @@
+const { response, request } = require('express');
+import request from "request";
+
 require('dotenv').config();
 
 let getHomePage = (req, res) => {
@@ -13,6 +16,15 @@ let postWebhook = (req, res) => {
         body.entry.foreach(function(entry) {
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            if(webhook_event.message){
+                handleMessage(sender_psid, webhook_event.message);
+            }else if (webhook_event.postback){
+                handlePostback(sender_psid, webhook_event.postback)
+            }
         })
         res.status(200).send("EVENT_RECEIVED");
     } else {
@@ -40,6 +52,42 @@ let getWebhook = (req, res) => {
         }
     }
 }
+
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    if(received_message.text){
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    }
+
+    callSendAPI(sender_psid, response);
+}
+
+function handlePostback(sender_psid, received_postback) {
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message":  response
+    }
+
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": {"access_token": process.env.PAGE_ACCESS_TOKEN},
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body)=> {
+        if(!err){
+            console.log('message sent')
+        }else{
+            console.log('Unable to send message:' + err);
+        }
+    })
+}
+
+function callSendAPI(sender_psid, response) {}
 
 module.exports = {
     getHomePage: getHomePage,
