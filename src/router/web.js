0d03ef8c34a48f75fcaabbd1b46ab724db519679
Nguyen Sy Kien <161858691+kienpo@users.noreply.github.com>
2024-12-28 10:18:20 +0700

    Update web.js

--- src/router/web.js
+++ src/router/web.js
@@ -5,6 +5,7 @@ let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
+    router.get("/setup-profile", homeController.setupProfile);
 
     router.post('/webhook', homeController.postWebhook);
     router.get('/webhook', homeController.getWebhook);
